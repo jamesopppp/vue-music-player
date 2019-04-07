e55c34dcacb71b77fc1259bb3419bf0a4d31ca77
james <jamessupermail@163.com>
2019-04-07 15:38:49 +0800

    :tada: Initialize Repo

--- src/router.js
+++ src/router.js
@@ -1,25 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+  routes: [{
+    path: '/',
+    redirect: 'recommend'
+  }, {
+    path: '/recommend',
+    name: 'recommend',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ './components/recommend/recommend.vue')
+  }, {
+    path: '/singer',
+    name: 'singer',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ './components/singer/singer.vue')
+  }, {
+    path: '/rank',
+    name: 'rank',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ './components/rank/rank.vue')
+  }, {
+    path: '/search',
+    name: 'search',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ './components/search/search.vue')
+  }]
 })
