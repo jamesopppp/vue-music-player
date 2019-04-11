8773c3bce56a75613d2557aa858a0161b7b57251
James's MacBook <jamessupermail@163.com>
2019-04-11 09:12:12 +0800

    封装listview 完善字母索引

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="singer" ref="singer">
-    <!-- <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
-    <router-view></router-view>-->
+    <list-view :data="singers" ref="list"></list-view>
+    <!--<router-view></router-view>-->
   </div>
 </template>
 
 <script>
-//   import ListView from 'base/listview/listview'
+import ListView from 'base/listview/listview'
 import { getSingerList } from 'api/singer'
 import { ERR_OK } from 'api/config'
-//   import Singer from 'common/js/singer'
+import Singer from 'common/js/singer'
 //   import {mapMutations} from 'vuex'
 //   import {playlistMixin} from 'common/js/mixin'
 
@@ -42,8 +42,7 @@ export default {
       getSingerList().then(res => {
         if (res.code === ERR_OK) {
           this.singers = res.data.list
-          console.log(this.singers)
-          // this.singers = this._normalizeSinger(res.data.list)
+          this.singers = this._normalizeSinger(res.data.list)
         }
       })
     },
@@ -57,62 +56,50 @@ export default {
 
       list.forEach((item, index) => {
         if (index < HOT_SINGER_LEN) {
-          map.hot.items.push({
-            id: item.Fsinger_mid,
-            name: item.Fsinger_name
-          })
+          map.hot.items.push(
+            new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            })
+          )
         }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(
+          new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          })
+        )
       })
+      // 处理map得到有序列表
+      const ret = []
+      const hot = []
+      for (const key in map) {
+        const val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
     }
-    // _normalizeSinger(list) {
-    //   let map = {
-    //     hot: {
-    //       title: HOT_NAME,
-    //       items: []
-    //     }
-    //   }
-    //   list.forEach((item, index) => {
-    //     if (index < HOT_SINGER_LEN) {
-    //       map.hot.items.push(new Singer({
-    //         name: item.Fsinger_name,
-    //         id: item.Fsinger_mid
-    //       }))
-    //     }
-    //     const key = item.Findex
-    //     if (!map[key]) {
-    //       map[key] = {
-    //         title: key,
-    //         items: []
-    //       }
-    //     }
-    //     map[key].items.push(new Singer({
-    //       name: item.Fsinger_name,
-    //       id: item.Fsinger_mid
-    //     }))
-    //   })
-    //   // 为了得到有序列表，我们需要处理 map
-    //   let ret = []
-    //   let hot = []
-    //   for (let key in map) {
-    //     let val = map[key]
-    //     if (val.title.match(/[a-zA-Z]/)) {
-    //       ret.push(val)
-    //     } else if (val.title === HOT_NAME) {
-    //       hot.push(val)
-    //     }
-    //   }
-    //   ret.sort((a, b) => {
-    //     return a.title.charCodeAt(0) - b.title.charCodeAt(0)
-    //   })
-    //   return hot.concat(ret)
-    // },
     // ...mapMutations({
     //   setSinger: 'SET_SINGER'
     // })
+  },
+  components: {
+    ListView
   }
-  // components: {
-  //   ListView
-  // }
 }
 
 //
